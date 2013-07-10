02f6f7382ae2e55832d033509d74b463ad0d505d
Ken Taylor <taylor.kenneth@gmail.com>
2013-07-10 20:13:09 +0000

    Added fs code

--- web.js
+++ web.js
@@ -1,10 +1,17 @@
 var express = require('express');
 
+var buffer = new Buffer('NO CONTENT');
+
+fs.readFile('index.html',function (err, data) {
+  if (err) throw err;
+  buffer = data;
+}
+
 var app = express.createServer(express.logger());
 
 // Modified this for the startup class
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;

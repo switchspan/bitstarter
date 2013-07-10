4f99d599003634b634ed4d8087ea42b693872220
Ken Taylor <taylor.kenneth@gmail.com>
2013-07-10 20:01:16 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+// Modified this for the startup class
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });

2957aa15c0680f0c9d98dbcdf2900015780c8cb4
Ken Taylor <taylor.kenneth@gmail.com>
2013-07-10 20:28:12 +0000

    Fixed buffer bug

--- web.js
+++ web.js
@@ -1,17 +1,12 @@
 var express = require('express');
 
-var buffer = new Buffer('NO CONTENT');
-
-fs.readFile('index.html',function (err, data) {
-  if (err) throw err;
-  buffer = data;
-}
+var buffer = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 // Modified this for the startup class
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;

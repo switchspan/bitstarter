ab011161c01d74703489721c2edbd0e2fe2d1b86
Ken Taylor <taylor.kenneth@gmail.com>
2013-07-10 20:33:15 +0000

    Still fixing fs bug

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
+var fs = require('fs');
 
-var buffer = fs.readFileSync('index.html');
+var buffer = fs.readFileSync('index.html','utf8');
 
 var app = express.createServer(express.logger());
 
